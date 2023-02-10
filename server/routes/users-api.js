bda7750c4dd930646b36fcf31b1191976ce9adab
Patrick Wolff <pat.wolff81@gmail.com>
2023-02-09 22:49:27 -0600

    Adding createUser for User API

--- server/routes/users-api.js
+++ server/routes/users-api.js
@@ -3,22 +3,22 @@
  Author: Patrick Wolff
  Contributors: April Yang, Andres Macias
  Date: 02/07/2023
- Description: Building security-questions APIs
+ Description: Building User APIs
  */
 
 const express = require("express");
 const User = require("../models/user");
-// building reusable APIs
-const ErrorResponse = require("../services/error-response");
-const BaseResponse = require("../services/base-response");
+const bcrypt = require('bcryptjs');
+const BaseResponse = require('../services/base-response');
+const ErrorResponse = require('../services/error-response');
 
 // import reuseable error messages from config.json
 const config = require("../data/config.json");
 
 const router = express.Router();
+const saltRounds = 10; // default salt rounds for hashing algorithm
 
 // Find all users, findAllUsers
-
 /**
  * @openapi
  * /api/users:
@@ -29,6 +29,13 @@ const router = express.Router();
  *     description: Reads,retrieves all users.
  *     summary: Returns all users.
  *     operationId: findAllUsers
+ *     parameters:
+ *       - name: users
+ *         in: path
+ *         required: true
+ *         description: Reads,retrieves all users.
+ *         schema:
+ *           type: string
  *     responses:
  *       '200':
  *         description: Returned all users
@@ -38,45 +45,67 @@ const router = express.Router();
  *         description: MongoDB Exception
  */
 router.get("/", async (req, res) => {
-  // find all users, or return an error message
   try {
-    User.find({}, function(err, users) {
-      /**
-       * TODO: Uncomment this code when we have more users and ready to test the API with the isDisabled flag
-       * Keep this commented out, api will not work with this code for now.
-       */
-      // .where("isDisabled")
-      // .equals(false)
-      // .exec(function (err, users) {
-        if (err) {
-          console.log(err);
-          const findAllMongoDBErrorResponse = new BaseResponse(
-            501,
-            `${config.mongoServerError}:${err.message}`,
-            null
-          );
-
-          console.log(findAllMongoDBErrorResponse.toObject());
-          res.status(501).send(findAllMongoDBErrorResponse.toObject());
-        } else {
-          const findAllResponse = new BaseResponse(
-            200,
-            `findAllUsers query was successful.`,
-            users
-          );
-          console.log(findAllResponse.toObject());
-          res.json(findAllResponse.toObject());
-        }
-      });
+    // find all users, or return an error message
+    User.find({}, function (err, users) {
+      if (err) {
+        console.log(err);
+        // if there is a mongodb error, handle it and return a 501 error message
+        res.status(501).send({
+          err: config.mongoServerError + ": " + err.message,
+        });
+      } else {
+        console.log(users);
+        res.json(users);
+      }
+    });
   } catch (e) {
+    console.log(e);
     // internal Server Error
-    const findAllErrorResponse = new ErrorResponse(
-      500,
-      `${config.serverError}:${err.message}`,
-      null
-    );
-    console.log(findAllErrorResponse.toObject());
-    res.status(500).send(findAllErrorResponse.toObject());
+    res.status(500).send({
+      err: config.serverError + ": " + err.message,
+    });
+  }
+});
+
+// Create User
+
+router.post('/', async(req, res) => {
+  try {
+    let hashedPassword = bcrypt.hashSync(req.body.password, saltRounds); // salt/hash the password
+
+    standardRole = {
+      text: 'standard'
+    }
+
+    // user object
+    let newUser = {
+      userName: req.body.userName,
+      password: hashedPassword,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      phoneNumber: req.body.phoneNumber,
+      address: req.body.address,
+      email: req.body.email,
+      role: standardRole
+    };
+
+    User.create(newUser, function(err, user) {
+      if (err) {
+        console.log(err);
+        const createUserMongodbErrorResponse = new ErrorResponse(500, 'Internal server error', err);
+        res.status(500).send(createUserMongodbErrorResponse.toObject());
+      } else {
+        console.log(user);
+        const CreateUserResponse = new BaseResponse(200, 'Query successful', user);
+        res.json(CreateUserResponse.toObject());
+      }
+    })
+
+  } catch (e) {
+    console.log(e);
+    const createUserCatchErrorResponse = ErrorResponse(500, 'Internal server error', e.message);
+    res.status(500).send(createUserCatchErrorResponse.toObject());
   }
 });
 
