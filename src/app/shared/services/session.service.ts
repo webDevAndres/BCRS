a8f334f2555d4db928ccdc1b8a51892973ce605e
webDevAndres <macias.andres02@gmail.com>
2023-02-17 23:02:12 -0600

    session.service.ts: add register and update password methods

--- src/app/shared/services/session.service.ts
+++ src/app/shared/services/session.service.ts
@@ -9,6 +9,7 @@ Description: Service for session management
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { User } from '../models/user.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -25,4 +26,34 @@ export class SessionService {
       password
     })
   }
+
+  /**
+   * Description: This function will call the register user api
+   */
+  register(user: User): Observable<any> {
+    return this.http.post('/api/session/register', {
+      userName: user.userName,
+      password: user.password,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      phoneNumber: user.phoneNumber,
+      address: user.address,
+      email: user.email,
+      selectedSecurityQuestions: user.selectedSecurityQuestions
+    })
+  }
+
+
+
+  /**
+   * Description: This function will call the API to update a user's password
+   */
+  updatePassword(password: string, userName: string): Observable<any> {
+    return this.http.put('/api/session/users/' + userName + '/password', {
+      password
+    })
+  }
+
+
 }
+
