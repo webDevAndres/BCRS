6129ed2ce3f0edd14351b7f4c53a2d497eaafabf
webDevAndres <macias.andres02@gmail.com>
2023-02-17 23:11:51 -0600

    selected security questions: fixed typo, add comments

--- src/app/shared/models/selected-security-question.interface.ts
+++ src/app/shared/models/selected-security-question.interface.ts
@@ -1,4 +1,13 @@
-export interface SelectedSecurityQuestion {
+/*
+Title: security-question.interface.ts
+Author: Professor Krasso
+Updated Date: 02/17/2023
+Modified By: Andres Macias/Patrick Wolff/April Yang
+Description: Security Question interface
+*/
+
+
+export interface SelectedSecurityQuestions {
   questionText: string;
   answerText: string;
 }
