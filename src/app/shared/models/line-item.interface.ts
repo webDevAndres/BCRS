0826f0d3698989535f197ef142d5bca53a1d1b6c
aprilyanggarwood <yangyyj822@hotmail.com>
2023-02-23 13:10:26 -0800

    created line-item.interface, role.interface, line-item schema

---
+++ src/app/shared/models/line-item.interface.ts
@@ -0,0 +1,12 @@
+/*
+Title: line-item.interface.ts
+Author: Professor Krasso
+Updated Date: 02/23/2023
+Modified By: Andres Macias/Patrick Wolff/April Yang
+Description: line-item interface
+*/
+
+export interface LineItem{
+  title: string;
+  price: number;
+}
---
+++ src/app/shared/models/role.interface.ts
@@ -0,0 +1,14 @@
+
+/*
+Title: role.interface.ts
+Author: Professor Krasso
+Updated Date: 02/23/2023
+Modified By: Andres Macias/Patrick Wolff/April Yang
+Description: role interface
+*/
+
+
+export interface Role{
+  _id?: string;
+  text: string;
+}
