fd0d0ded1ba19fc6cb9674b108d7ab70f5ed6bbb
aprilyanggarwood <yangyyj822@hotmail.com>
2023-02-25 08:09:47 -0800

    completed invoice.ts

--- src/app/shared/models/invoice.ts
+++ src/app/shared/models/invoice.ts
@@ -10,5 +10,60 @@ Description: Invoice model
 import { LineItem } from "./line-item.interface";
 
 export class Invoice {
+  private username: string;
+  private lineItems: LineItem[];
+  private orderDate: string;
+  private LABOR_RATE: number = 50;
+
+  partsAmount: number;
+  laborHours: number;
+
+  constructor(username?: string, partsAmount?: number, laborHours?: number) {
+    this.username = username || '';
+    this.partsAmount = partsAmount || 0;
+    this.laborHours = laborHours || 0;
+    this.orderDate = new Date().toLocaleDateString();
+    this.lineItems = [];
+  }
+
+  getUsername(): string{
+    return this.username;
+  }
+
+  setLineItems(lineItems: LineItem[]): void{
+    this.lineItems = lineItems;
+  }
+
+  getLineItems(): LineItem[]{
+    return this.lineItems;
+  }
+
+  // shopping cart
+  getLineItemTotal(): number{
+    let total: number = 0;
+    for (let lineItem of this.lineItems) {
+      total += lineItem.price;
+    }
+    return Number(total);
+  }
+
+
+  getLaborAmount(): number{
+    return Number(this.laborHours) * Number(this.LABOR_RATE);
+  }
+
+  getOrderDate(): string{
+    return this.orderDate;
+  }
+
+  getTotal(): number{
+    return Number(this.partsAmount) + Number(this.getLaborAmount()) + Number(this.getLineItemTotal());
+  }
+
+  clear() {
+    this.partsAmount = 0;
+    this.laborHours = 0;
+    this.lineItems = [];
+  }
 
 }
