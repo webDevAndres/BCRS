95441255ee4c4c91d50d468f74d981eda4303f2b
webDevAndres <macias.andres02@gmail.com>
2023-02-27 21:40:59 -0600

    invoice service: added createInvoice method

--- src/app/shared/services/invoice.service.ts
+++ src/app/shared/services/invoice.service.ts
@@ -21,22 +21,37 @@ export class InvoiceService {
 
   constructor(private http:HttpClient) { }
 
-  placeOrder(items: Product[]) {
-    // create an invoice object
-    let lineItems = [];
-    for (let i = 0; i < items.length; i++) {
-      let lineItem = { title: items[i].title, price: items[i].price, laborFee: items[i].laborFee };
-      lineItems.push(lineItem);
-    }
-    let invoice = new Invoice();
-    invoice.setLineItems(lineItems);
+  // placeOrder(items: Product[]) {
+  //   // create an invoice object
+  //   let lineItems = [];
+  //   for (let i = 0; i < items.length; i++) {
+  //     let lineItem = { title: items[i].title, price: items[i].price, laborFee: items[i].laborFee };
+  //     lineItems.push(lineItem);
+  //   }
+  //   let invoice = new Invoice();
+  //   invoice.setLineItems(lineItems);
 
-    // insert that object in the mongoDB collection
-    console.log("posting order to mongo");
-    return this.http.post(`/api/invoices`, {body: invoice});
+  //   // insert that object in the mongoDB collection
+  //   console.log("posting order to mongo");
+  //   return this.http.post(`/api/invoices`, {body: invoice});
 
+  // }
+
+  createInvoice(userName: string, invoice: Invoice): Observable<any> {
+    return this.http.post(`/api/invoices/${userName}`, {
+      userName: userName,
+      lineItems: invoice.getLineItems(),
+      partsAmount: invoice.partsAmount,
+      laborAmount: invoice.getLaborAmount(),
+      lineItemTotal: invoice.getLineItemTotal(),
+      total: invoice.getTotal()
+    });
   }
-  findPurchaseByServiceGraph(): Observable<any>{
+
+
+
+
+  findPurchasesByServiceGraph(): Observable<any>{
     return this.http.get(`/api/invoices/purchases-graph`);
   }
 
