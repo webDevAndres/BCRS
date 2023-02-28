c6ee3f1f9668dd4546cabd9ce7456b0db342972c
webDevAndres <macias.andres02@gmail.com>
2023-02-27 21:39:26 -0600

    service-repair: add code for cart

--- src/app/pages/service-repair/service-repair.component.ts
+++ src/app/pages/service-repair/service-repair.component.ts
@@ -8,12 +8,17 @@ Description: service-repair component
 
 
 import { Component, OnInit } from '@angular/core';
+import { LineItem } from 'src/app/shared/models/line-item.interface';
 import { Product } from 'src/app/shared/models/product.interface';
-import { ProductService } from 'src/app/shared/services/product.service';
-import { CartService } from 'src/app/shared/services/cart.service';
-import { ActivatedRoute } from '@angular/router';
+import { Invoice } from 'src/app/shared/models/invoice';
+import { Message } from 'primeng/api';
 import { CookieService } from 'ngx-cookie-service';
-
+import { Router } from '@angular/router';
+import { ProductService } from 'src/app/shared/services/product.service';
+import { InvoiceService } from 'src/app/shared/services/invoice.service';
+import { MatDialog } from '@angular/material/dialog';
+import { CartService } from 'src/app/shared/services/cart.service';
+// import { ActivatedRoute } from '@angular/router';
 
 
 @Component({
@@ -24,39 +29,50 @@ import { CookieService } from 'ngx-cookie-service';
 export class ServiceRepairComponent implements OnInit {
 
   // descriptions!: Array<any>
-  products: Array<any>;
+  username: string;
+  products: Product[]; //this is the items in the cart
+  lineItems: LineItem[];
+  invoice: Invoice;
+  errorMessages: Message[];
+  successMessages: Message[];
+
   sessionUserName: string;
 
 
   constructor(
     private productService: ProductService,
-    private route: ActivatedRoute,
     private cartService: CartService,
-    private cookieService: CookieService,
-
+    private cookieService: CookieService
   ) {
-    // populate products that are listed in the product.service.ts to the html
-    this.products = this.productService.getProducts();
-    // only seesion user can add product to the shopping cart
+
+    this.username = this.cookieService.get('sessionuser') || '';
+    this.products = [];
+    this.lineItems = [];
+    this.invoice = {} as Invoice;
+    this.errorMessages = [];
+    this.successMessages = [];
+
+    // only session user can add product to the shopping cart
     this.sessionUserName = this.cookieService.get('sessionuser');
+
+    this.products = this.productService.getProducts();
+    this.invoice = new Invoice(this.username);
+
+    console.log(this.products);
   }
 
   // the product only can be added to the shopping cart once with alert messages
-addToCart(product: Product) {
-  let successfullyAdded = this.cartService.addToCart(product);
-  if (successfullyAdded) {
-    window.alert('Your product has been added to the cart!');
-  } else {
-    window.alert("Product has already been added.");
+  addToCart(product: Product) {
+    let successfullyAdded = this.cartService.addToCart(product);
+    if (successfullyAdded) {
+      window.alert('Your product has been added to the cart!');
+    } else {
+      window.alert("Product has already been added.");
+    }
   }
- }
-
-
 
   ngOnInit(): void {
 
   }
-
-
-
 }
+
