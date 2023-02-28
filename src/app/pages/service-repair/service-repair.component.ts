af0e42a4bcdb3b1543e7f26944e81b9e0d0d8615
webDevAndres <macias.andres02@gmail.com>
2023-02-28 13:23:42 -0600

    add cookies to addToCart

--- src/app/pages/service-repair/service-repair.component.ts
+++ src/app/pages/service-repair/service-repair.component.ts
@@ -8,14 +8,11 @@ Description: service-repair component
 
 
 import { Component, OnInit } from '@angular/core';
-import { LineItem } from 'src/app/shared/models/line-item.interface';
 import { Product } from 'src/app/shared/models/product.interface';
-import { Invoice } from 'src/app/shared/models/invoice';
 import { Message } from 'primeng/api';
 import { CookieService } from 'ngx-cookie-service';
 import { Router } from '@angular/router';
 import { ProductService } from 'src/app/shared/services/product.service';
-import { InvoiceService } from 'src/app/shared/services/invoice.service';
 import { MatDialog } from '@angular/material/dialog';
 import { CartService } from 'src/app/shared/services/cart.service';
 // import { ActivatedRoute } from '@angular/router';
@@ -31,8 +28,7 @@ export class ServiceRepairComponent implements OnInit {
   // descriptions!: Array<any>
   username: string;
   products: Product[]; //this is the items in the cart
-  lineItems: LineItem[];
-  invoice: Invoice;
+  currentCount: number;
   errorMessages: Message[];
   successMessages: Message[];
 
@@ -47,28 +43,28 @@ export class ServiceRepairComponent implements OnInit {
 
     this.username = this.cookieService.get('sessionuser') || '';
     this.products = [];
-    this.lineItems = [];
-    this.invoice = {} as Invoice;
     this.errorMessages = [];
     this.successMessages = [];
+    this.currentCount = Number(this.cookieService.get('cartCount') || 0);
 
     // only session user can add product to the shopping cart
     this.sessionUserName = this.cookieService.get('sessionuser');
 
     this.products = this.productService.getProducts();
-    this.invoice = new Invoice(this.username);
-
-    console.log(this.products);
   }
 
   // the product only can be added to the shopping cart once with alert messages
   addToCart(product: Product) {
-    let successfullyAdded = this.cartService.addToCart(product);
-    if (successfullyAdded) {
-      window.alert('Your product has been added to the cart!');
-    } else {
-      window.alert("Product has already been added.");
-    }
+    this.currentCount = this.cartService.itemsCount() + 1;
+
+
+    //set cookie for the cart items
+    this.cookieService.set('cartItems', JSON.stringify(this.cartService.addToCart(product)), 1);
+    //set cookie for the cart count
+    this.cookieService.set('cartCount', JSON.stringify(this.currentCount), 1);
+
+    // used to add the product to the shopping cart
+    this.cartService.addToCart(product);
   }
 
   ngOnInit(): void {
